7ddd64bc49fd1a99b9325617d028b834d3183968
Vishrut_Pradhan <117522822+VishrutPradhan@users.noreply.github.com>
2024-04-30 19:43:04 +0530

    Add files via upload
    signup files

---
+++ signup/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDy_C6iTvOY9zvb3lkApCPHge08LrlHaZE",
+  authDomain: "chatapp-421812.firebaseapp.com",
+  projectId: "chatapp-421812",
+  storageBucket: "chatapp-421812.appspot.com",
+  messagingSenderId: "969384020713",
+  appId: "1:969384020713:web:f204dcefb8b7ef9d133e78"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
